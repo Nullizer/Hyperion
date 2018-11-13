deff85591660ead0b1b196f68139da9f3fc41e4c
Nullizer <nullizer@gmail.com>
2018-11-14 04:45:09 +0800

    Use memo and StrictMode; Modify Heading impl

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Fragment, lazy, Suspense } from 'react'
+import { Fragment, lazy, Suspense, StrictMode, memo } from 'react'
 import { render } from 'react-dom'
 import styles from './article.scss'
 import Clock from './Clock'
@@ -9,21 +9,21 @@ const Toggle = lazy(() => import('./Toggle'))
 const root = document.createElement('div')
 document.body.insertBefore(root, document.body.firstElementChild)
 
-const Heading = () =>
+const Heading = memo((props: {name: string}) =>
   <Fragment>
     <h1>Hello, world!</h1>
-    <h2>Long Live JSX!</h2>
-  </Fragment>
+    <h2>Long Live {props.name}!</h2>
+  </Fragment>)
 
 render(
-  <Fragment>
-    <Heading />
+  <StrictMode>
+    <Heading name='JSX' />
     <p className={styles.paragraphTheme}>Hyperion 796</p>
     <Clock />
     <Suspense fallback={<div>Loading...</div>}>
       <Toggle />
     </Suspense>
-  </Fragment>,
+  </StrictMode>,
   root
 )
 
